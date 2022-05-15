7010d7edf3ee3d3dad24a4c531cd918fdbedae34
Ahmed Hassan <atariq344@gmail.com>
2022-05-15 22:32:41 +0500

    submission

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import AnimatedText from 'react-animated-text-content';
+
 import './header.css';
 import headerlogo from '../../Assets/PNG/Header_logo.png'
 const Header = () => {
@@ -6,9 +8,15 @@ const Header = () => {
     <div className='header_container'>
       <div className='header_title_container'>
         <div className='header_title'>
-          <h1>HEADING TAGLINE</h1>
-          <h1>LOREM IPSUM</h1>
-          <h1>LOREM IPSUM</h1>
+        <AnimatedText className='animationss' children='ReactText' type='words' interval={0.04}  duration={0.4} animation={{ y: '100px', ease: 'ease', type:'float' }}>
+        Hello!
+        </AnimatedText>
+        <AnimatedText className='animationss' children='ReactText' type='words' interval={0.04}  duration={0.} animation={{ y: '100px', ease: 'ease', type:'float' }}>
+        I'm Ahmed Hassan
+        </AnimatedText>
+          {/* <h1>HEADING TAGLINE</h1> */}
+          {/* <h1>LOREM IPSUM</h1> */}
+          {/* <h1>LOREM IPSUM</h1> */}
           <button className='header_button'>MEET ME</button>
           <p>Lorem ipsLorem ipsum dolor sit amet, consectetur adipiscing elit. 
           In sed sem et tortor rutrum venenatis. 
@@ -17,7 +25,7 @@ const Header = () => {
         
       </div>
       <div className='header_image_container'>
-        <img src={headerlogo} alt='header_logo'/>
+        <img className='header_hex' src={headerlogo} alt='header_logo'/>
       </div>
 
     </div>
